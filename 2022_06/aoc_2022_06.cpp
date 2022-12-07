0a74fd9815c7e61954070c5dc99cec55d16566b4
unknown <jrhagelin@gmail.com>
2022-12-07 10:28:13 +0100

    Day five refactoring

--- 2022_06/aoc_2022_06.cpp
+++ 2022_06/aoc_2022_06.cpp
@@ -4,72 +4,56 @@
 #include <vector>
 #include <queue>
 
-auto get_lines() -> std::vector<std::string> {
+auto get_line() -> std::string {
   std::ifstream input_file("input.txt");
 
   if (!input_file.is_open())
     std::cout << "Error opening file" << std::endl;
 
-  std::vector<std::string> lines;
   std::string line;
-  while (std::getline(input_file, line))
-    lines.push_back(line);
+  std::getline(input_file, line);
 
-  input_file.close();
-  return lines;
+  return line;
 }
 
 auto all_deque_chars_different(const std::deque<char>& queue) -> bool {
-  for (auto c : queue) {
+  for (auto c : queue) 
     if (std::count(queue.begin(), queue.end(), c) > 1)
       return false;
-  }
   return true;
 }
 
-auto part_one(const std::vector<std::string>& lines) -> void {
-  for (auto line : lines) {
-    std::deque<char> queue;
-    int char_index = 0;  
-    for (auto c : line) {
-      queue.push_back(c);
+auto count_of_chars_before_n_unique_chars(const std::string& line, int n) -> int {
+  std::deque<char> queue;
+  int chars_processed = 0;
 
-      if (queue.size() > 4)
-        queue.pop_front();
-      
-      if (queue.size() == 4 && all_deque_chars_different(queue)) 
-          break;
+  for (auto c : line) {
+    ++chars_processed;
+    queue.push_back(c);
 
-      ++char_index;
-    }
-
-    std::cout << "Part one: chars processed: " << char_index + 1 << std::endl;
+    if (queue.size() > n)
+      queue.pop_front();
+    
+    if (queue.size() == n && all_deque_chars_different(queue)) 
+        break;
   }
+
+  return chars_processed;
 }
 
-auto part_two(const std::vector<std::string>& lines) -> void {
-  for (auto line : lines) {
-    std::deque<char> queue;
-    int char_index = 0;  
-    for (auto c : line) {
-      queue.push_back(c);
+auto part_one(const std::string line) -> void {
+  std::cout << "Part one: chars processed: ";
+  std::cout << count_of_chars_before_n_unique_chars(line, 4) << std::endl;
+}
 
-      if (queue.size() > 14)
-        queue.pop_front();
-      
-      if (queue.size() == 14 && all_deque_chars_different(queue)) 
-          break;
-
-      ++char_index;
-    }
-
-    std::cout << "Part two: chars processed: " << char_index + 1 << std::endl;
-  }
+auto part_two(const std::string line) -> void {
+  std::cout << "Part two: chars processed: ";
+  std::cout << count_of_chars_before_n_unique_chars(line, 14) << std::endl;
 }
 
 int main() {
-  auto lines = get_lines();
-  part_one(lines);
-  part_two(lines);
+  auto line = get_line();
+  part_one(line);
+  part_two(line);
   return 0;
 }
